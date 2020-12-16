d7d37d8965412019126433d3096897f47f17a3c6
Nivedita Bauskar <nivedita.bauskar@lntinfotech.com>
2020-12-16 09:24:11 +0530

    cs project

---
+++ sampleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
